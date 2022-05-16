4b027cff9793b76c1112eb2abb30d8e744e31f97
Ivan <github@ivandean.com>
2022-05-16 11:17:50 +1000

    Some fixes as Bourne/Dash doesn't have $RANDOM
    /bin/sh is linked to "dash" on Ubuntu, details here: https://wiki.ubuntu.com/DashAsBinSh

--- bash/ping-randomly.sh
+++ bash/ping-randomly.sh
@@ -8,11 +8,19 @@ RUNMAX=10
 
 while [ 1 ]; do
   rnd1=$RANDOM
+  if [ -z "$rnd1" ]  # Bourne, Dash don't have $RANDOM
+  then
+    rnd1=`hexdump -n 2 -e '/2 "%u"' /dev/urandom`
+  fi
   rnd2=$RANDOM
+  if [ -z "$rnd2" ]
+  then
+    rnd2=`hexdump -n 2 -e '/2 "%u"' /dev/urandom`
+  fi
   
   sleepTime=`expr $rnd1 % $SLEEPMAX`
   runTime=`expr $rnd2 % $RUNMAX`
-  runTime=`expr $runTime +1` # Because 0 means forever
+  runTime=`expr $runTime + 1` # Because 0 means forever
   
   echo "Running ping for $runTime seconds then sleeping for $sleepTime"
   pktSize=$RANDOM
